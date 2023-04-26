f83a14a80eb961b01a858e2527504970eb19d169
Erhan <erhantezer68@gmail.com>
2023-04-26 14:04:27 +0300

    router edited

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,36 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import NotFound from "../components/NotFound";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import SingleCocktail from "../pages/SingleCocktail";
 
 
 export const router = createBrowserRouter([
     {
         element: (
-                <Navbar/>
-            )
+            <>
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </>
+        ),
+        errorElement: <NotFound />,
+        children: [
+            {
+                path:"/",
+                element: <Home/>
+            },
+            {
+                path:"about",
+                element: <About/>
+            },
+            {
+                path:"cocktail/:id",
+                element: <SingleCocktail/>
+            },
+        
+        ]
     }
 ])
