309f71848a1168ca8a97a040a1fcdbd9f0fb597d
Erhan <erhantezer68@gmail.com>
2023-04-26 16:56:08 +0300

    footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,25 @@
 
 
 const Footer = () => {
+    
     return (
         <>
             <footer className="">
                 <div className="social-icons">
-                    <a href="#twitter" rel="noopener noreferrer" target='true'>
+                    <a href="https://twitter.com" rel="noopener noreferrer" target='true'>
                         <i className="fab fa-twitter fa-2x"></i>
                     </a>
-                    <a href="#facebook" rel="noopener noreferrer" target='true'>
+                    <a href="https://facebook.com" rel="noopener noreferrer" target='true'>
                         <i className="fab fa-facebook fa-2x"></i>
                     </a>
-                    <a href="#instagram" rel="noopener noreferrer" target='true'>
+                    <a href="https://instagram.com" rel="noopener noreferrer" target='true'>
                         <i className="fab fa-instagram fa-2x"></i>
                     </a>
-                    <a href="#github" rel="noopener noreferrer" target='true'>
+                    <a href="https://github.com" rel="noopener noreferrer" target='true'>
                         <i className="fab fa-github fa-2x"></i>
                     </a>
                 </div>
-                <div className="copyright">&copy; Copyright {new Date().getFullYear()}</div>
+                <div className="copyright">Created By Erhan TEZER &copy; Copyright {new Date().getFullYear()}</div>
             </footer>
         </>
     )
