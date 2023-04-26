9cb8dd05cc42be45a976956aea740a13cdcb31bc
Erhan <erhantezer68@gmail.com>
2023-04-26 16:16:22 +0300

    navbar and css updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+import logo from "../assets/logo.svg";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="navbar">
+            <div className="nav-center">
+                <Link to="/">
+                    <img src={logo} alt="cocktail logo" className="logo" />
+                </Link>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="about">about</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
