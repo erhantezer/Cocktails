82150e7916f2cd87f2a14f5d49c247e3f5126dc5
Erhan <erhantezer68@gmail.com>
2023-04-26 13:39:29 +0300

    updated  footer and navbar

---
+++ src/router/router.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import { createBrowserRouter } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+    {
+        element: (
+                <Navbar/>
+            )
+    }
+])
