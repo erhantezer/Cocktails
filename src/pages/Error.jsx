c412ba2601681c0bc77f27925b78e554d663aadc
Erhan <erhantezer68@gmail.com>
2023-04-27 11:48:50 +0300

    updated error pages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>Error</div>
+        <section className='error-page section'>
+            <div className='error-container'>
+                <h1>OOPS! IT'S A DEAD END</h1>
+                <Link to="/" className='btn btn-primary'>
+                    back home
+                </Link>
+            </div>
+        </section>
     )
 }
 
